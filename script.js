d177eb406462363f41a9f71b7ea8258e9fe3a791
felipeoliveiracode <felipeoliveiramidias@gmail.com>
2025-01-02 19:06:54 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
 
-function newTime(){
+function newTime() {
     const date = new Date();
-    
+
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    
+
     hoursElement.textContent = fixTime(hours);
     minutesElement.textContent = fixTime(minutes);
     secondsElement.textContent = fixTime(seconds);
 }
 
 // RETIRANDO O ERRO DO "0"
-function fixTime(time){
-    return time <10 ? '0'+time : time
+function fixTime(time) {
+    return time < 10 ? '0' + time : time
 }
 
 // ATUALIZANDO RELOGIO EM 1S 
